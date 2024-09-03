23928c399bb3c589463facef6cb08f4767a71851
Mithumlahiru <mithumlahiru1031@gmail.com>
2024-09-04 01:12:51 +0530

    comment hardcoded event post and HTML tags Retrive data form databse and display it in event page

--- events.php
+++ events.php
@@ -1,28 +1,47 @@
 <?php include 'assets/header.php'; ?>
+<?php include 'connection.php' ?>
 
-    <!-- Event Page  -->
-    <div class="event-container">
-        <div class="event-title">
-            <h1>Events & News</h1><br>
-        </div>
+<?php
+//Define SQL query to get stored in the database
+$sql = "SELECT event_title, event_images, event_content FROM events";
 
-        <div class="event-post post1">   <!--first event post-->
-            <h3>FACETS 2023: A GLOBAL INVITATION TO THE MOST FABULOUS GEM SHOW IN SRI LANKA</h3>
-            <h4>by Kavidu Hasaranga &nbsp; June 28,2024</h4>
-            <br>
-            <div class="event-img img1">
-                <img src="Images/event_post1.jpg">
-            </div>
-            <br>
-            <p>In the heart of the Indian Ocean, where the allure of gemstones meets the enchanting landscapes of Sri Lanka, the stage is set for the ...</p>
-            <br>
-            <span>Posted in <a href="#">Blue sapphire</a>, <a href="#">Events</a>, <a href="#">Gem Exhibition</a>, <a href="#">gemstone</a>, <a href="#">sustainability</a></span>
-            <br><br>
-            <a href="#"><button class="read-more">Read More</button></a>
-        </div>
-        <br>
-        <div class="event-post post2">   <!--second event post-->
-            <h3>HONG KONG JEWELRY & GEM FAIR 2023 18TH-22ND SEPTEMBER BOOTH NUMBER AWE 2N09</h3>
+// Execute the query
+$result = $conn->query($sql);
+
+echo '<div class="event-container">'; 
+    echo '<div class="event-title">';
+    echo '<h1>Events & News</h1>'.'<br>'; 
+    echo '</div>';
+
+// Check if there are any results
+if ($result->num_rows > 0){
+
+    // Output data of each row
+    while ($row = $result->fetch_assoc()){
+        echo '<div class="event-post post1">'; 
+        echo '<h3>'.htmlspecialchars($row["event_title"]).'</h3>';
+        echo '<h4>'.date('Y-m-d H:i:s').'</h4>';
+            echo '<br>';
+            echo '<div class="event-img img1">';
+                echo '<img src="data:image/jpeg;base64,' . base64_encode($event_images) . '" alt="Event Image" />';
+            echo '</div>';
+            echo '<br>';
+            echo '<p>'.(htmlspecialchars($row["event_content"])).'</p>';
+            echo '<br>';
+            echo '<span> Posted in <a href="#">Blue sapphire</a>, <a href="#">Events</a>, <a href="#">Gem Exhibition</a>, <a href="#">gemstone</a>, <a href="#">sustainability</a></span>';
+            echo '<br><br>';
+            echo '<a href="#"><button class="read-more">Read More</button></a>';
+        echo '</div>';
+        echo '<br>'; 
+    }
+}
+// Close the connection
+$conn->close();
+?>
+
+    
+        <!-- <div class="event-post post2">   second event post -->
+            <!-- <h3>HONG KONG JEWELRY & GEM FAIR 2023 18TH-22ND SEPTEMBER BOOTH NUMBER AWE 2N09</h3>
             <h4>by Sangeeth Thisaranga &nbsp; June 07,2024</h4>
             <br>
             <div class="event-img img2">
@@ -35,9 +54,9 @@
             <br><br>
             <a href="#"><button class="read-more">Read More</button></a>
         </div>
-        <br>
-        <div class="event-post post3">   <!--thrid event post-->
-            <h3>68TH BANGKOK GEMS & JEWELRY FAIR 2023-BOOTH NUMBER T34</h3>
+        <br> -->
+        <!-- <div class="event-post post3">   thrid event post -->
+            <!-- <h3>68TH BANGKOK GEMS & JEWELRY FAIR 2023-BOOTH NUMBER T34</h3>
             <h4>by Randeesh Weerasinghe &nbsp; April 15,2024</h4>
             <br>
             <div class="event-img img3">
@@ -50,9 +69,9 @@
             <br><br>
             <a href="#"><button class="read-more" height="100px">Read More</button></a>
         </div>
-        <br>
-        <div class="event-post post4">   <!--fourth event post-->
-            <h3>FACETS'24 SRI LANKA INTERNATIONAL GEM & JEWELLERY</h3>
+        <br> -->
+        <!-- <div class="event-post post4">   fourth event post -->
+            <!-- <h3>FACETS'24 SRI LANKA INTERNATIONAL GEM & JEWELLERY</h3>
             <h4>by Mithum Lahiru &nbsp; December 20,2023</h4>
             <br>
             <div class="event-img img4">
@@ -65,9 +84,9 @@
             <br><br>
             <a href="#"><button class="read-more">Read More</button></a>
         </div>
-        <br>
-        <div class="event-post post5">   <!--fifth event post-->
-            <h3>PREMIER INTERNATIONAL GEM SHOW 2024 IN SRI LANKA</h3>
+        <br> -->
+        <!-- <div class="event-post post5">   fifth event post -->
+            <!-- <h3>PREMIER INTERNATIONAL GEM SHOW 2024 IN SRI LANKA</h3>
             <h4>by Kamal Perera &nbsp; January 01,2024</h4>
             <br>
             <div class="event-img img5">
@@ -83,6 +102,6 @@
             <a href="#"><button class="read-more">Read More</button></a>
         </div>
         <br>
-    </div>
+    </div> -->
 
 <?php include 'assets/footer.php'; ?>
