15b8c7a696dabfb084e292c16e91d029809c8873
Mithumlahiru <mithumlahiru1031@gmail.com>
2024-09-16 12:52:16 +0530

    create get data that stored in gemstore_db and display it in event and blog pages

--- blog.php
+++ blog.php
@@ -1,8 +1,32 @@
 <?php include 'assets/header.php'; ?>
 
+<?php
+// Define your SQL query
+$sql = "SELECT title, content FROM blog";
 
+// Execute the query
+$result = $conn->query($sql);
 
+// Check if there are any results
+if ($result->num_rows > 0) {
 
+    // Output data of each row
+    while ($row = $result->fetch_assoc()) {
+        // Convert newlines to <p> tags
+        $content = '<p>' . str_replace("\n", '</p><p>', $row["content"]) . '</p>';
 
+        echo '<section class="blog-posts">';
+            echo '<div id="postsContainer">';
+            echo '<h3>' . htmlspecialchars($row["title"]) .'</h3>';
+            echo '<div class="para_event">' . $content .'</div>';
+            echo '</div>';
+            echo '<br>';
+            echo '<hr>';
+        echo '</section>';
+    }
+} 
+// Close the connection
+$conn->close();
+?>
 
 <?php include 'assets/footer.php'; ?>
--- events.php
+++ events.php
@@ -51,4 +51,62 @@
         </div>
     </div>
 
+<?php
+//Define SQL query to get stored in the database
+$sql = "SELECT event_title, event_images, event_content, created_at FROM events";
+
+// Execute the query
+$result = $conn->query($sql);
+
+    echo '<div class="event-container">';
+    echo '<div class="event-title">';
+    echo '<h1>Events & News</h1>'.'<br>'; 
+    echo '</div>';
+    echo '<div class="event-post post2">';
+            echo '<h3>HONG KONG JEWELRY & GEM FAIR 2023 18TH-22ND SEPTEMBER BOOTH NUMBER AWE 2N09</h3>';
+            echo '<h4>Posted on: 2024-08-22</h4>';
+            echo '<br>';
+            echo '<div class="event-img img2">';
+                echo '<img src="Images/event_post2.jpg">';
+            echo '</div>';
+            echo '<br>';
+            echo '<p>World’s Number One Fine Jewellery Event Returns In 2023!Jewellery & Gem WORLD Hong Kong (JGW), popularly known as the September Hong Kong Jewel...</p>';
+            echo '<br>';
+            echo '<span>Posted in <a href="#">Blue sapphire</a>, <a href="#">Events</a>, <a href="#">Gem Exhibition</a>, <a href="#">gemstone</a>, <a href="#">sustainability</a></span>';
+            echo '<br><br>';
+            echo '<a href="#"><button class="read-more">Read More</button></a>';
+        echo '</div>';
+
+// Check if there are any results
+if ($result->num_rows > 0){
+
+    // Output data of each row
+    while ($row = $result->fetch_assoc()){
+        echo '<div class="event-post post1">'; 
+        echo '<h3>'.htmlspecialchars($row["event_title"]).'</h3>';
+        echo '<h4>Posted on: '.date('Y-m-d', strtotime($row["created_at"])).'</h4>';; //display date that stored data in database
+            echo '<br>';
+            echo '<div class="event-img img1">';
+            if ($row["event_images"]) {
+                echo '<img src="data:image/jpeg;base64,' . base64_encode($row["event_images"]) . '" alt="Event Image" />'; //display event post image
+            } else {
+                echo '<p>No image available</p>';
+            }
+            echo '</div>';
+            echo '<br>';
+            // Convert newlines to <p> tags
+            $content = '<p>' . str_replace("\n", '</p><p>', $row["event_content"]) . '</p>';
+            echo '<div>' . $content . '</div>'; //display event content
+            echo '<br>';
+            echo '<span> Posted in <a href="#">Blue sapphire</a>, <a href="#">Events</a>, <a href="#">Gem Exhibition</a>, <a href="#">gemstone</a>, <a href="#">sustainability</a></span>';
+            echo '<br><br>';
+            echo '<a href="#"><button class="read-more">Read More</button></a>';
+        echo '</div>';
+        echo '<br>'; 
+    }
+}
+// Close the connection
+$conn->close();
+?>
+
 <?php include 'assets/footer.php'; ?>
